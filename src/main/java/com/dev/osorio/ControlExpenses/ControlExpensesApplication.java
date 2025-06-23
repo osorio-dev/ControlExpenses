ab3fc0240e9f0b90f8b2ead24f4878509af792fa
osorio-dev <feeh.osorio.dev@gmail.com>
2025-06-22 22:26:05 -0300

    first commit - Init Application

---
+++ src/main/java/com/dev/osorio/ControlExpenses/ControlExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.osorio.ControlExpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/osorio/ControlExpenses/ControlExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.osorio.ControlExpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
