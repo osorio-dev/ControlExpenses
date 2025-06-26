ce29ed58a1e1aa5de3ed92dc35783a2d1c44ca54
osorio-dev <feeh.osorio.dev@gmail.com>
2025-06-26 17:07:01 -0300

    feat: created controller and configured some endpoints

---
+++ src/main/java/com/dev/osorio/ControlExpenses/controllers/ExpenseController.java
@@ -0,0 +1,55 @@
+package com.dev.osorio.ControlExpenses.controllers;
+
+import com.dev.osorio.ControlExpenses.entitys.ExpenseModel;
+import com.dev.osorio.ControlExpenses.services.ExpenseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/expense")
+public class ExpenseController {
+
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping("/allexpenses")
+    public ResponseEntity<List<ExpenseModel>> getAllExpenses() {
+        return ResponseEntity.ok(expenseService.getAllExpenses());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExpenseModel> getExpenseById(@PathVariable("id") Long id) {
+        ExpenseModel expenseModel = expenseService.getExpenseById(id);
+
+        if (expenseModel == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.ok(expenseModel);
+    }
+
+    @GetMapping("/date")
+    public List<ExpenseModel> getExpensesByDateTime(@RequestParam("month") int month) {
+        List<ExpenseModel> expenseModelList = expenseService.getAllExpensesByDateTime(month);
+
+        return expenseModelList;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ExpenseModel> saveExpense(@RequestBody ExpenseModel expenseModel) {
+        return ResponseEntity.ok(expenseService.saveExpense(expenseModel));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteExpenseById(@PathVariable("id") Long id) {
+        expenseService.deleteExpenseById(id);
+
+        return "Despesa Deletada com Sucesso!!";
+    }
+}
