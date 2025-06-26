637a67be7fdf043b5cb574a147001bf5e07d8cb1
osorio-dev <feeh.osorio.dev@gmail.com>
2025-06-26 17:08:50 -0300

    feat: created service and logic for requests

---
+++ src/main/java/com/dev/osorio/ControlExpenses/services/ExpenseService.java
@@ -0,0 +1,50 @@
+package com.dev.osorio.ControlExpenses.services;
+
+import com.dev.osorio.ControlExpenses.entitys.ExpenseModel;
+import com.dev.osorio.ControlExpenses.repositorys.ExpenseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    //Get All Expenses
+    public List<ExpenseModel> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    //Get Expense per ID
+    public ExpenseModel getExpenseById(Long id) {
+        Optional<ExpenseModel> expenseModel = expenseRepository.findById(id);
+
+        return expenseModel.orElse(null);
+    }
+
+    //Get Filter Expense DateTime
+    public List<ExpenseModel> getAllExpensesByDateTime(int month) {
+
+        return expenseRepository.findByDate(month);
+    }
+
+    //Post Save Expense
+    public ExpenseModel saveExpense(ExpenseModel expenseModel) {
+        return expenseRepository.saveAndFlush(expenseModel);
+    }
+
+    //Delete Expense
+    public void deleteExpenseById(Long id) {
+        Optional<ExpenseModel> expenseModel = expenseRepository.findById(id);
+
+        expenseModel.ifPresent(expense -> {
+            expenseRepository.deleteById(id);
+        });
+    }
+}
