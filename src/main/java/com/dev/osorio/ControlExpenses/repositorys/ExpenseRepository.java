27c0e8e4d4f13fd35c754d3fea315339b73e9895
osorio-dev <feeh.osorio.dev@gmail.com>
2025-06-26 17:10:22 -0300

    feat: created repository and created a custom query

--- src/main/java/com/dev/osorio/ControlExpenses/repositorys/ExpenseRepository.java
+++ src/main/java/com/dev/osorio/ControlExpenses/repositorys/ExpenseRepository.java
@@ -2,8 +2,15 @@ package com.dev.osorio.ControlExpenses.repositorys;
 
 import com.dev.osorio.ControlExpenses.entitys.ExpenseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<ExpenseModel, Long> {
+
+    @Query("SELECT e FROM ExpenseModel e WHERE EXTRACT(MONTH FROM e.dateTime) = :month")
+    List<ExpenseModel> findByDate(@Param("month") int month);
 }
