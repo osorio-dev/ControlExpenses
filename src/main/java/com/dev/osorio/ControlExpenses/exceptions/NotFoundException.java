bd521f7d05629e8bc4b8eb92b08903ee2c4afead
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-06-30 00:10:37 -0300

    feat: created exceptions and a controllerAdvice to better handle how to manage exceptions

---
+++ src/main/java/com/dev/osorio/ControlExpenses/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.dev.osorio.ControlExpenses.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(){}
+
+
+    public NotFoundException(String message){super(message);}
+}
