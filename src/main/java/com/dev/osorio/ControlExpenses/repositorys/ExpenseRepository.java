5ed4b4d4871fc3139a502237dd0ae2c4f9c9b8ad
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-07-02 04:26:55 -0300

    feat: create custom querys findByName, findByCategory and findByDate

--- src/main/java/com/dev/osorio/ControlExpenses/repositorys/ExpenseRepository.java
+++ src/main/java/com/dev/osorio/ControlExpenses/repositorys/ExpenseRepository.java
@@ -6,11 +6,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<ExpenseModel, Long> {
 
-    @Query("SELECT e FROM ExpenseModel e WHERE EXTRACT(MONTH FROM e.dateTime) = :month")
-    List<ExpenseModel> findByDate(@Param("month") int month);
+
+    List<ExpenseModel> findByName(String name);
+
+    List<ExpenseModel> findByCategory(String category);
+
+    @Query("SELECT e FROM ExpenseModel e WHERE e.dateTime >= :startDay AND e.dateTime < :startDayTomorrow")
+    List<ExpenseModel> findByDate(
+            @Param("startDay") LocalDateTime startDay,
+            @Param("startDayTomorrow") LocalDateTime startDayTomorrow
+    );
 }
