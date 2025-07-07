5df49f38df97e01c71b909b284dfd37b63b0289d
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-07-07 00:13:30 -0300

    feat: created exception to capture invalid date entries in database searches

---
+++ src/main/java/com/dev/osorio/ControlExpenses/exceptions/InvalidInputException.java
@@ -0,0 +1,8 @@
+package com.dev.osorio.ControlExpenses.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
